e0b872aaebf8276dcfa90e00e64610e1c490bced
Paul Cahill <cahill.paul@verizon.net>
2025-05-27 07:46:04 -0400

    init commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/cahillpa/azure_api_demo/AzureApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cahillpa.azure_api_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureApiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cahillpa/azure_api_demo/AzureApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cahillpa.azure_api_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
