0d634004cc0ea48f1a677f54eb48836d1c97cc11
Paul Cahill <cahill.paul@verizon.net>
2025-05-29 11:05:52 -0400

    corrected build and deploy error

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,17 +25,33 @@ resource "azurerm_service_plan" "azure_api_demo_plan" {
 
 resource "azurerm_linux_function_app" "azure_api_demo_function" {
   name                = "azure-api-demo-function"
-  resource_group_name = azurerm_resource_group.azure_api_demo_rg.name
   location            = azurerm_resource_group.azure_api_demo_rg.location
+  resource_group_name = azurerm_resource_group.azure_api_demo_rg.name
   service_plan_id     = azurerm_service_plan.azure_api_demo_plan.id
-  storage_account_name = azurerm_storage_account.azure_api_demo_storage.name
+  storage_account_name       = azurerm_storage_account.azure_api_demo_storage.name
+  storage_account_access_key = azurerm_storage_account.azure_api_demo_storage.primary_access_key
 
   site_config {
     application_stack {
       java_version = "17"
     }
+
     always_on     = true
     http2_enabled = true
   }
 
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"      = "java"
+    "FUNCTIONS_EXTENSION_VERSION"   = "~4"
+    "JAVA_VERSION"                  = "17"
+    "WEBSITE_RUN_FROM_PACKAGE"      = "1"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  https_only = true
 }
+
